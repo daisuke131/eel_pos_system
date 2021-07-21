46fd8873ce92f2fb967a69191bcd732d7e034e09
daisuke <dada.00131@gmail.com>
2021-07-21 11:13:24 +0900

    追加ボタン実装

--- pos_system.py
+++ pos_system.py
@@ -3,7 +3,7 @@ import os
 # import sys
 from datetime import datetime
 
-import eel
+# import eel
 import pandas as pd
 
 datetime_now = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
@@ -13,18 +13,18 @@ RECEIPT_PATH = os.path.join(os.getcwd(), f"receipt/{datetime_now}.txt")
 
 # 商品クラス
 class Item:
-    def __init__(self, item_code: str, item_name: str, price: int):
+    def __init__(self, item_code: str, item_name: str, price: int) -> None:
         self.item_code: str = item_code
         self.item_name: str = item_name
         self.price: int = price
 
-    def get_price(self):
-        return self.price
+    # def get_price(self):
+    #     return self.price
 
 
 # オーダークラス
 class Order:
-    def __init__(self, item_master):
+    def __init__(self, item_master) -> None:
         self.item_order_list = []
         self.item_amount_list = []
         self.total_price: int = 0
@@ -33,77 +33,86 @@ class Order:
     def fetch_item_data(self, item_code: str):
         for item in self.item_master:
             if item.item_code == item_code:
-                item_data = {
-                    "item_code": item.item_code,
-                    "item_name": item.item_name,
-                    "price": item.price,
-                }
-                return item_data
+                # item_data = {
+                #     "item_code": item.item_code,
+                #     "item_name": item.item_name,
+                #     "price": item.price,
+                # }
+                return item.item_name, item.price
 
-    def add_item_order(self, item_code: str, item_amount: int):
-        self.item_order_list.append(item_code)
-        self.item_amount_list.append(item_amount)
-        eel.output_oder_list(f"{item_code}が{item_amount}個")
-        # print("買い物かごに追加しました。")
+    def add_item_order(self, item_code: str, item_amount: int) -> bool:
+        item_data = self.fetch_item_data(item_code)
+        if item_data:
+            self.item_order_list.append(item_code)
+            self.item_amount_list.append(item_amount)
+            return True
+            # eel.output_oder_list(f"{item_code}が{item_amount}個")
+        else:
+            # eel.not_exist_item("指定した商品が見つかりません。")
+            return False
 
-    def input_order(self):
-        while True:
-            # 商品コード入力
-            order_item_code: str = input("注文したい商品コードを入力してください。未入力で注文処理へ。>>")
-            order_item_code = order_item_code.strip()
-            if order_item_code.strip():
-                if self.item_exists(order_item_code):
-                    # 数量入力
-                    while True:
-                        order_item_count: str = input("数量を入力してください。>>")
-                        order_item_count = order_item_count.strip()
-                        try:
-                            order_item_count_int: int = int(order_item_count)
-                            break
-                        except Exception:
-                            print("数量を正しく入力してください。")
-                            pass
-                    # オーダーリストに追加
-                    self.add_item_order_list(order_item_code, order_item_count_int)
-                else:
-                    print("商品コードに誤りがあります。")
-                    continue
-            else:
-                print("清算処理へ移ります。")
-                break
-
-    def item_exists(self, order_item_code: str):
-        for item in self.item_master:
-            if item.item_code == order_item_code:
-                return True
-        return False
+    # def input_order(self):
+    #     while True:
+    #         # 商品コード入力
+    #         order_item_code: str = input("注文したい商品コードを入力してください。未入力で注文処理へ。>>")
+    #         order_item_code = order_item_code.strip()
+    #         if order_item_code.strip():
+    #             if self.item_exists(order_item_code):
+    #                 # 数量入力
+    #                 while True:
+    #                     order_item_count: str = input("数量を入力してください。>>")
+    #                     order_item_count = order_item_count.strip()
+    #                     try:
+    #                         order_item_count_int: int = int(order_item_count)
+    #                         break
+    #                     except Exception:
+    #                         print("数量を正しく入力してください。")
+    #                         pass
+    #                 # オーダーリストに追加
+    #                 self.add_item_order_list(order_item_code, order_item_count_int)
+    #             else:
+    #                 print("商品コードに誤りがあります。")
+    #                 continue
+    #         else:
+    #             print("清算処理へ移ります。")
+    #             break
 
     # def add_item_order(self, item_code: str):
     #     self.item_order_list.append(item_code)
 
-    def view_item_list(self):
+    def output_oder_list(self) -> str:
+        output_oder_text: str = "==========ご注文リスト==========\n"
+        self.total_price = 0
+        count = 0
         for order_item_code, order_count in zip(
             self.item_order_list, self.item_amount_list
         ):
             item_data = self.fetch_item_data(order_item_code)
-            item_total_price: int = int(item_data["price"]) * int(order_count)
-            self.write_receipt("商品コード：{}".format(item_data["item_code"]))
-            self.write_receipt("商品名：{}".format(item_data["item_name"]))
-            self.write_receipt("金額：{}".format("{:,}".format(item_data["price"])))
-            self.write_receipt("数量：{}".format("{:,}".format(order_count)))
-            self.write_receipt("小計：￥{}円".format("{:,}".format(item_total_price)))
-            self.write_receipt("===================================")
+            item_total_price: int = int(item_data[1]) * int(order_count)
             self.total_price += item_total_price
-        self.write_receipt("合計：￥{}円".format("{:,}".format(self.total_price)))
-        print("合計：￥{}円になります。".format("{:,}".format(self.total_price)))
+            count += 1
+            # self.write_receipt("商品コード：{}".format(item_data["item_code"]))
+            # self.write_receipt("商品名：{}".format(item_data["item_name"]))
+            # self.write_receipt("金額：{}".format("{:,}".format(item_data["price"])))
+            # self.write_receipt("数量：{}".format("{:,}".format(order_count)))
+            # self.write_receipt("小計：￥{}円".format("{:,}".format(item_total_price)))
+            # self.write_receipt("===================================")
+            output_oder_text += (
+                f"{count}.商品名:{item_data[0]} 値段:{item_data[1]}円 個数:{order_count}個\n"
+            )
+        # self.write_receipt("合計：￥{}円".format("{:,}".format(self.total_price)))
+        output_oder_text += "=============================\n"
+        # print(f"合計：￥{"{:,}".format(self.total_price)}円になります。")
+        output_oder_text += f"合計：￥{self.total_price:,}円"
+        return output_oder_text
 
-    def write_receipt(self, write_text: str):
-        with open(
-            RECEIPT_PATH,
-            mode="a",
-            encoding="utf_8_sig",
-        ) as f:
-            f.write(write_text + "\n")
+    # def write_receipt(self, write_text: str):
+    #     with open(
+    #         RECEIPT_PATH,
+    #         mode="a",
+    #         encoding="utf_8_sig",
+    #     ) as f:
+    #         f.write(write_text + "\n")
 
     def pay_off(self):
         while True:
@@ -195,7 +204,4 @@ class PosSystem:
             return False
 
     def init_order(self):
-        """
-        オーダーを初期化する
-        """
         self.order = Order(self.item_master)
